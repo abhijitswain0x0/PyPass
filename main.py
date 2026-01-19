9087dce390679460ebb61bd3d3fb9e9ae1ceff36
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-19 19:18:06 +0530

    remove unused import 'sys'

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 characters_uppercase = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
