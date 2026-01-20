34b3e4b3e91263a93a55441ba220ad3d069048a4
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-20 18:00:41 +0530

    add main function for user expirence improvment by asking for input

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ standard_password_length = 16
 current_password_lenght = 0
 password = []
 
-def create_password():
+def create_password(standard_password_length = standard_password_length, current_password_lenght = current_password_lenght):
     while standard_password_length > current_password_lenght:
         append_array = random.randrange(0, 5)
 
@@ -36,3 +36,31 @@ def create_password():
             password.append(charecter_map.symbols[0])
 
         current_password_lenght += 1
+
+
+def main() :
+    print("Password Genrator")
+    
+    choice = input("Genrate Password [Y/N]: ")
+
+    avalaible_inputs = ["y", "Y", "Yes", "YES", "yes"]
+
+    if choice == avalaible_inputs[0]:
+        create_password()
+        return(password)
+    elif choice == avalaible_inputs[1]:
+        create_password()
+        return(password)
+    elif choice == avalaible_inputs[2]:
+        create_password()
+        return(password)
+    elif choice == avalaible_inputs[3]:
+        create_password()
+        return(password)
+    elif choice == avalaible_inputs[4]:
+        create_password()
+        return(password)
+    else:
+        return("The program has been terminated by user")
+
+print(main())
