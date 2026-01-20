ff8c3bd27e6bdf7aac82352e3dcf11e1f45547b1
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-20 14:17:42 +0530

    create funvton create_password to encapsulate the password creator

--- main.py
+++ main.py
@@ -20,22 +20,19 @@ class charecter_map:
 
 standard_password_length = 16
 current_password_lenght = 0
-
 password = []
 
-while standard_password_length > current_password_lenght:
-    append_array = random.randrange(0, 5)
-    
-    if append_array == 1:
-        password.append(charecter_map.characters_uppercase[random.randrange(0, 26)])
-    elif append_array == 2: 
-        password.append(charecter_map.characters_lowercase[random.randrange(0, 26)])
-    elif append_array == 3: 
-        password.append(charecter_map.numbers[random.randrange(0, 9)])
-    elif append_array == 4: 
-        password.append(charecter_map.symbols[0])
+def create_password():
+    while standard_password_length > current_password_lenght:
+        append_array = random.randrange(0, 5)
 
-    current_password_lenght += 1
+        if append_array == 1:
+            password.append(charecter_map.characters_uppercase[random.randrange(0, 26)])
+        elif append_array == 2: 
+            password.append(charecter_map.characters_lowercase[random.randrange(0, 26)])
+        elif append_array == 3: 
+            password.append(charecter_map.numbers[random.randrange(0, 9)])
+        elif append_array == 4: 
+            password.append(charecter_map.symbols[0])
 
-print(password)
-
+        current_password_lenght += 1
