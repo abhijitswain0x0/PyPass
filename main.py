486b8fb423dbc25fe1563de9c79d03dc9e122587
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-20 13:18:22 +0530

    implement better password genration for scalability using if else blocks and loops to reach desired password lenght

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ characters_uppercase = [
     ]
 
 characters_lowercase = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" , "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    "a", "b", "append_array", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" , "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     ]
 
 numbers = [
@@ -21,3 +21,19 @@ standard_password_length = 16
 current_password_lenght = 0
 
 password = []
+
+while standard_password_length > current_password_lenght:
+    append_array = random.randrange(0, 5)
+    
+    if append_array == 1:
+        password.append(characters_uppercase[random.randrange(0, 26)])
+    elif append_array == 2: 
+        password.append(characters_lowercase[random.randrange(0, 26)])
+    elif append_array == 3: 
+        password.append(numbers[random.randrange(0, 9)])
+    elif append_array == 4: 
+        password.append(symbols[0])
+
+    cpl += 1 
+
+print(password)
