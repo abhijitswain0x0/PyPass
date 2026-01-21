cee263ea35b4ec7b1abec4972eb807cf82f236c1
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-21 18:04:18 +0530

    delete charecter_map class and all arrays in it and transfer to charecter_map.py. import charecter_map.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 import random
-
-class charecter_map: 
-    characters_uppercase = [
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-        ]
-
-    characters_lowercase = [
-        "a", "b", "append_array", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" , "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-        ]
-
-    numbers = [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9"
-        ]
-
-    symbols = [
-        "-"
-        ]
-
+import charecter_map
 
 standard_password_length = 16
 current_password_lenght = 0
