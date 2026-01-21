e3cbb5cac8056691aa524280bac1358d93e757f0
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-01-21 18:02:40 +0530

    Created charecter_map.py and transfered all charecter maps from main.py to charecter_map.py

---
+++ charecter_map.py
@@ -0,0 +1,16 @@
+class charecter_map: 
+    characters_uppercase = [
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        ]
+
+    characters_lowercase = [
+        "a", "b", "append_array", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" , "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        ]
+
+    numbers = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        ]
+
+    symbols = [
+        "-"
+        ]
