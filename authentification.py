65b5e2814a8071a98a0bd049d7ccd5f6fdc28ef6
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-02-24 19:06:16 +0530

    add error catching

--- authentification.py
+++ authentification.py
@@ -1,12 +1,20 @@
 def auth():
     master_password = "master_password"
+    
+    try:
+        provided_master_password = input("Enter Master Password: ")
 
-    provided_master_password = input("Enter Master Password: ")
+        if provided_master_password == master_password:
+            print("Password verified!")
+        
+            return True
 
-    if provided_master_password == master_password:
-        print("Confirmed")
+        elif provided_master_password != master_password:
+            print("Wrong Password!")
+        
+            return False
 
-    elif provided_master_password != master_password:
-        print("Error")
+    except KeyboardInterrupt:
+        print("\nExiting...")
 
-auth()
+        exit()
