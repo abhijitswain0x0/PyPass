376e49c0e30d7c8e9e6ff322b94752d8350bcaaf
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-03-03 11:52:15 +0530

    add comments explaining the auth system

--- authentification.py
+++ authentification.py
@@ -1,20 +1,25 @@
 def auth():
-    master_password = "master_password"
+    master_password = "master_password"  # stores the master passwords
 
+    # try except block for error handling
     try:
-        provided_master_password = input("Enter Master Password: ")
+        provided_master_password = input(
+            "Enter Master Password: "
+        )  # asks the user to give the master password
 
+        # checks if the provided_master_password = master_password
+        #
         if provided_master_password == master_password:
             print("Password verified!")
 
-            return True
+            return True  # returns True as confirmation
 
         elif provided_master_password != master_password:
             print("Wrong Password!")
 
             return False
 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:  # listens for keyboard interrupt and handles it correctly by calling python's in-built exit() function
         print("\nExiting...")
 
         exit()
