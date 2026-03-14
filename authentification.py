01d8430f49c6b1b63f3c0077fbdcc05ec3a06cab
Abhijit Swain <abhijitswain0x0@gmail.com>
2026-03-14 10:32:52 +0530

    add logic to check if a user with a password exists if not then create one and set a password for it

--- authentification.py
+++ authentification.py
@@ -1,27 +1,65 @@
+from pathlib import Path
+import json
+
+
 def authentificate_user():
-    master_password = "master_password"  # stores the master passwords
 
-    # try except block for error handling
-    try:
-        provided_master_password = input(
-            "Enter Master Password: "
-        )  # asks the user to give the master password
+    user_exists = if_user_exists()
 
-        # checks if the provided_master_password = master_password
-        #
-        if provided_master_password == master_password:
-            print("Password verified!")
+    if user_exists:
 
-            return True  # returns True as confirmation
+        path = Path("passwords/master_password.json")
+        data = json.loads(path.read_text())
 
-        elif provided_master_password != master_password:
-            print("Wrong Password!")
+        master_password = data["sudo"]
 
-            return False
+        # try except block for error handling
+        try:
+            provided_master_password = input(
+                "Enter Master Password: "
+            )  # asks the user to give the master password
 
-    except (
-        KeyboardInterrupt
-    ):  # listens for keyboard interrupt and handles it correctly by calling python's in-built exit() function
-        print("\nExiting...")
+            # checks if the provided_master_password = master_password
 
-        exit()
+            if provided_master_password == master_password:
+                print("Password verified!")
+
+                return True  # returns True as confirmation
+
+            elif provided_master_password != master_password:
+                print("Wrong Password!")
+
+                return False
+
+        except (
+            KeyboardInterrupt
+        ):  # listens for keyboard interrupt and handles it correctly by calling python's in-built exit() function
+            print("\nExiting...")
+
+            exit()
+
+    else:
+        create_user()
+
+
+def if_user_exists():
+    master_password = Path("passwords/master_password.json")
+
+    if master_password.is_file():
+        return True
+
+    else:
+        return False
+
+
+def create_user():
+    print("No user has been identified!" + "\nCreating new user...")
+
+    master_password = input("Set a master password:")
+
+    master_password_json = {"sudo": master_password}
+
+    master_password_file_path = Path("Passwords/master_password.json")
+
+    master_password_file_path.parent.mkdir(parents=True, exist_ok=True)
+    master_password_file_path.write_text(json.dumps(master_password_json, indent=4))
